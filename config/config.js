f2671eaf0e0325204cab7cd4703a2476981d02f0
zhangxu <zhangxu@ybm100.com>
2019-10-28 13:55:53 +0800

    修改数据库密码

--- config/config.js
+++ config/config.js
@@ -5,7 +5,7 @@ module.exports = {
         host: 'localhost',
         port: 3306,
         user: 'root',
-        password: 'zhangxu'
+        password: 'ZHANGXU'
     },
     security: {
         secretKey: "secretKey",
